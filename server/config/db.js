65159d3b9794a2ad7e2c0f2dec73025202de2123
Neelesh <121629275+neeleshXAIXdev@users.noreply.github.com>
2026-04-06 10:05:16 +0530

    Update database connection string in db.js
    Replaced MONGO_URI environment variable with direct MongoDB connection string.

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    // console.log(process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://devNeelesh:mongoDb1998@ac-1kpiwqz-shard-00-00.ufheuol.mongodb.net:27017,ac-1kpiwqz-shard-00-01.ufheuol.mongodb.net:27017,ac-1kpiwqz-shard-00-02.ufheuol.mongodb.net:27017/?ssl=true&replicaSet=atlas-n7qhbi-shard-0&authSource=admin&appName=Cluster0");
     console.log("Connected to DB");
   } catch (error) {
     console.error(error.message);
