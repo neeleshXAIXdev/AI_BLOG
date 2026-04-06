049aa6d489fbefc11b209b578a61df50e92da5d6
Neelesh <121629275+neeleshXAIXdev@users.noreply.github.com>
2026-04-06 10:08:13 +0530

    Update MongoDB connection string to use environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://devNeelesh:mongoDb1998@ac-1kpiwqz-shard-00-00.ufheuol.mongodb.net:27017,ac-1kpiwqz-shard-00-01.ufheuol.mongodb.net:27017,ac-1kpiwqz-shard-00-02.ufheuol.mongodb.net:27017/?ssl=true&replicaSet=atlas-n7qhbi-shard-0&authSource=admin&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB");
   } catch (error) {
     console.error(error.message);
